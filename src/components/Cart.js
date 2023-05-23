fb7672bf8286627d076d2ae23af5bffb87676bdb
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 13:52:50 -0400

    Cart file

---
+++ src/components/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <ul>
+        <li>
+          <img src="product1.png" alt="Product 1" />
+          <h3>Product 1</h3>
+          <p>$10.00</p>
+          <button>Remove</button>
+        </li>
+        <li>
+          <img src="product2.png" alt="Product 2" />
+          <h3>Product 2</h3>
+          <p>$20.00</p>
+          <button>Remove</button>
+        </li>
+        <li>
+          <img src="product3.png" alt="Product 3" />
+          <h3>Product 3</h3>
+          <p>$30.00</p>
+          <button>Remove</button>
+        </li>
+      </ul>
+      <p>Total: $60.00</p>
+      <button>Checkout</button>
+    </div>
+  );
+}
+
+export default Cart;
