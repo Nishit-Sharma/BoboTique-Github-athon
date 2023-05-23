06deea1e2823c8ab95ef70c38331d5a99766811f
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 11:45:57 -0400

    mall.js

---
+++ src/components/Mall.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Mall() {
+  return (
+    <div>
+      <h2>Mall Name</h2>
+      <ul>
+        <li><a href="#">Store 1</a></li>
+        <li><a href="#">Store 2</a></li>
+        <li><a href="#">Store 3</a></li>
+        <li><a href="#">Store 4</a></li>
+      </ul>
+    </div>  
+    );
+}
+
+export default Mall;
