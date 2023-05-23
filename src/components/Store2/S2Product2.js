fc551a591ae5392d183ccc0ac5128fec0cde5e3f
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 13:44:14 -0400

    god help me

---
+++ src/components/Store2/S2Product2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function S2Product2() {
+  return (
+    <div>
+      <h2>Product Name</h2>
+      <img src="product.png" alt="Product" />
+      <p>Description of the product.</p>
+      <p>$10.00</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+
+export default S2Product2;
---
+++ src/components/Store3/S3Product2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function S3Product2() {
+  return (
+    <div>
+      <h2>Product Name</h2>
+      <img src="product.png" alt="Product" />
+      <p>Description of the product.</p>
+      <p>$10.00</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+
+export default S3Product2;
