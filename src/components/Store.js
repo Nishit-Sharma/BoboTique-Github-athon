54d2e3d080648598b3c64c5307b241741007e12c
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 11:54:22 -0400

    Store files

---
+++ src/components/Store.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Store() {
+  return (
+    <div>
+      <h2>Store Name</h2>
+      <ul>
+        <li>
+          <img src="product1.png" alt="Product 1" />
+          <h3>Product 1</h3>
+          <p>$10.00</p>
+        </li>
+        <li>
+          <img src="product2.png" alt="Product 2" />
+         <h3>Product 2</h3>
+          <p>$20.00</p>
+        </li>
+        <li>
+          <img src="product3.png" alt="Product 3" />
+          <h3>Product 3</h3>
+          <p>$30.00</p>
+        </li>
+        <li>
+          <img src="product4.png" alt="Product 4" />
+          <h3>Product 4</h3>
+          <p>$40.00</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Store;
