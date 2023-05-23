d7b57a9a6898714f1154a53eb0ed560d0c26864d
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 11:26:56 -0400

    header file

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <header>
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Malls</a></li>
+            <li><a href="#">Cart</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
