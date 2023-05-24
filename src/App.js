e1dc1352bebedeceb37cad47462fde0c254dadb9
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 22:12:53 -0400

    revert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import Home from "./components/Home.js";
-import GlobalStyles from './components/Global.css';
 import './App.css';
 
 function App() {
-  //<GlobalStyles></GlobalStyles>;
   <Home></Home>;
 }
 
