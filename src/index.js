40d9b5885922b0eda72a076432b95fa4b0b6f201
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 10:58:36 -0400

    asf

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './components/Global.css';
+//import './components/Global.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
