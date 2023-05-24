779144b35277df1d03b5c1aea6d68049ec651f69
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 11:00:40 -0400

    asf

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Store1 from './Store1/Store.js'
 import Store2 from './Store2/Store2.js'
 import Store3 from './Store3/Store3.js'
 import Store4 from './Store4/Store4.js'
-import './Global.css';
+//import './Global.css';
 import NavBar from './Navbar.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
