6040fe8c15aa7d967aa17477d06f00ec229dbcf2
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 21:47:10 -0400

    idk

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import './Global.css';
 import Store1 from './Store1/Store.js'
 import Store2 from './Store2/Store2.js'
 import Store3 from './Store3/Store3.js'
@@ -11,7 +11,7 @@ function Home() {
       <h1>Welcome to our shopping website!</h1>
       <p>Choose from our four malls to start shopping:</p>
       <ul>
-        <li><a href="./Store1/Store.js">Mall 1</a></li>
+        <li><a href="Store1/Store.js">Mall 1</a></li>
         <li><a href="#">Mall 2</a></li>
         <li><a href="#">Mall 3</a></li>
         <li><a href="#">Mall 4</a></li>
