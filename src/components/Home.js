23cfe0c9176f270643df323715377e7b2ae4ce32
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-23 21:42:16 -0400

    ?

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../App.css';
-import Store1 from './components/Store1/Store.js'
-import Store2 from './components/Store2/Store2.js'
-import Store3 from './components/Store3/Store3.js'
-import Store4 from './components/Store4/Store4.js'
+import Store1 from './Store1/Store.js'
+import Store2 from './Store2/Store2.js'
+import Store3 from './Store3/Store3.js'
+import Store4 from './Store4/Store4.js'
 
 function Home() {
   return (
@@ -11,7 +11,7 @@ function Home() {
       <h1>Welcome to our shopping website!</h1>
       <p>Choose from our four malls to start shopping:</p>
       <ul>
-        <li><a href="./components/Store1/Store.js">Mall 1</a></li>
+        <li><a href="./Store1/Store.js">Mall 1</a></li>
         <li><a href="#">Mall 2</a></li>
         <li><a href="#">Mall 3</a></li>
         <li><a href="#">Mall 4</a></li>
