8e4f30c8b9ca1f212a391d678658b45b9cf0cea3
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 14:43:47 -0400

    header?

--- src/components/Home.js
+++ src/components/Home.js
@@ -48,7 +48,18 @@ function Home() {
 
   return (
     <div>
-      <head></head>
+      <header>
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Malls</a></li>
+            <li><a href="#">Cart</a></li>
+          </ul>
+        </nav>
+      </header>
       <h1>Welcome to our shopping website!</h1>
       <p>Choose from our four malls to start shopping:</p>
       <ul>
