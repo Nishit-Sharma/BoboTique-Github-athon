a6ce15dd3562f5a109a88d61ba9e78a82dc127f2
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 13:39:59 -0400

    whats wrong man

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import Home from "./components/Home.js";
-import GlobalStyles from './components/Global.css';
 import './App.css';
 import './components/Global.css';
 
