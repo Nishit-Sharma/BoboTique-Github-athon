5ebed2175ea1072e064235c0cba3fd296d9ba2db
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 19:49:57 -0400

    i forgot semicolon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import Home from "./components/Home.js";
-import Cart from "./components/Cart.js"
+import Cart from "./components/Cart.js";
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import './components/Global.css';
