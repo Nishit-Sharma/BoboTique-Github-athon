4d8d504fe4268dc464823bd255be1e061984818c
Nishit-Sharma <nishit.sharma@mcvts.org>
2023-05-25 09:50:50 -0400

    hello

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Global.css';
 import image from './icooked.png';
 
 function Header() {
-    return (
-      <header>
-        <div className="logo">
-          <Link to ="/"></Link>
-          <img href="#" src={image} alt="BoboTique" />
-        </div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
+  return (
+    <header>
+      <div className="logo">
+        <Link to="/">
+          <img src={image} alt="BoboTique" />
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li className="home">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="cart">
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
