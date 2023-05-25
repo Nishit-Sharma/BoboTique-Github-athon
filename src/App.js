a5517e0bdf6f73e77e8194f98476db9397c567b3
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-24 20:13:16 -0400

    transitions?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Home from "./components/Home.js";
 import Cart from "./components/Cart.js";
 import Store1 from "./components/Store1/Store.js";
@@ -13,16 +13,24 @@ import './components/Global.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="mall1" element={<Store1 />} />
-        <Route path="mall2" element={<Store2 />} />
-        <Route path="mall3" element={<Store3 />} />
-        <Route path="mall4" element={<Store4 />} />
-      </Routes>
-  </BrowserRouter>
+  <TransitionGroup>
+    <CSSTransition
+      key={location.key}
+      classNames="fade"
+      timeout
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="mall1" element={<Store1 />} />
+            <Route path="mall2" element={<Store2 />} />
+            <Route path="mall3" element={<Store3 />} />
+            <Route path="mall4" element={<Store4 />} />
+          </Routes>
+      </BrowserRouter>
+    </CSSTransition>
+  </TransitionGroup>
   );
 }
 
