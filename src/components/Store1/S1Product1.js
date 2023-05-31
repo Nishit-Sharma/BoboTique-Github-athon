303022cc91ce19080fc839e4664e1fd09ec8056d
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 12:06:18 -0400

    doing thing

--- src/components/Store1/S1Product1.js
+++ src/components/Store1/S1Product1.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../Global.css';
 import {useSpring, animated} from 'react-spring'
 import Layout from '../Layout.js'; 
+import { Mall, Item } from '../MallScript';
 
 function S1Product1() {
   const props = useSpring({
@@ -10,6 +11,13 @@ function S1Product1() {
     delay: 300,
   });
 
+  const myMall = new Mall("My Mall");
+
+  const handleAddToCart = () => {
+    const newItem = new Item("Product Name", 10.00);
+    myMall.addItem(newItem);
+  }
+
   return (
     <animated.div style = {props}>
       <Layout>
@@ -17,7 +25,7 @@ function S1Product1() {
         <img src="product.png" alt="Product" />
         <p>Description of the product.</p>
         <p>$10.00</p>
-        <button>Add to Cart</button>
+        <button onClick={handleAddToCart}>Add to Cart</button>
       </Layout>
     </animated.div>
   );
