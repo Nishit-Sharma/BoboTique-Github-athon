811a80e5feedd3b1b3a51ef52f01f28921d51cd1
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 11:39:23 -0400

    what da

---
+++ src/components/MallScript.js
@@ -0,0 +1,59 @@
+class Item {
+    constructor(name, price) {
+        this.itemName = name;
+        this.itemPrice = price;
+    }
+
+    getName() {
+        return this.itemName;
+    }
+
+    getPrice() {
+        return this.itemPrice;
+    }
+}
+
+class Mall {
+    constructor(mallName) {
+        this.mallName = mallName;
+        this.itemList = [];
+    }
+
+    getMallName() {
+        return this.mallName;
+    }
+
+    displayItems() {
+        for (const item of this.itemList) {
+            console.log(item.getName() + ": $" + item.getPrice());
+        }
+    }
+
+    addItem(item) {
+        this.itemList.push(item);
+    }
+
+    addItemAtIndex(item, index) {
+        this.itemList.splice(index, 0, item);
+    }
+
+    removeItem(index) {
+        this.itemList.splice(index, 1);
+    }
+
+    getItemName(index) {
+        return this.itemList[index].getName();
+    }
+
+    getItemPrice(index) {
+        return this.itemList[index].getPrice();
+    }
+
+    getItem(index) {
+        return this.itemList[index];
+    }
+
+    setItem(name, price, index) {
+        this.itemList[index] = new Item(name, price);
+    }
+}
--- src/components/Store1/Store.js
+++ src/components/Store1/Store.js
@@ -3,6 +3,7 @@ import Layout from '../Layout.js';
 import { Link } from 'react-router-dom';
 import '../Global.css';
 import {useSpring, animated} from 'react-spring'
+import { Mall, Item } from './MallScript';
 
 function Store() {
   const props = useSpring({
@@ -11,32 +12,25 @@ function Store() {
     delay: 300,
   });
 
+  const myMall = new Mall("My Mall");
+  myMall.addItem(new Item("Product 1", 10));
+  myMall.addItem(new Item("Product 2", 20));
+  myMall.addItem(new Item("Product 3", 30));
+  myMall.addItem(new Item("Product 4", 40));
+
   return (
     <animated.div style={props}>
       <Layout>
-      <h2>Store Name</h2>
-      <ul>
-        <li>
-          <img src="product1.png" alt="Product 1" />
-          <h3><Link to="/mall1/product1">Product 1</Link></h3>
-          <p>$10.00</p>
-        </li>
-        <li>
-          <img src="product2.png" alt="Product 2" />
-         <h3><Link to="/mall1/product2" >Product 2</Link></h3>
-          <p>$20.00</p>
-        </li>
-        <li>
-          <img src="product3.png" alt="Product 3" />
-          <h3><Link to="/mall1/product3">Product 3</Link></h3>
-          <p>$30.00</p>
-        </li>
-        <li>
-          <img src="product4.png" alt="Product 4" />
-          <h3><Link to="/mall1/product4">Product 4</Link></h3>
-          <p>$40.00</p>
-        </li>
-      </ul>
+        <h2>{myMall.getMallName()}</h2>
+        <ul>
+          {myMall.itemList.map((item, index) => (
+            <li key={index}>
+              <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
+              <h3><Link to={`/mall1/product${index + 1}`}>{item.getName()}</Link></h3>
+              <p>${item.getPrice().toFixed(2)}</p>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </animated.div>
   );
