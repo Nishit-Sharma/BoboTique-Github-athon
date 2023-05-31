9e63e5f5da3ed95e3de6fa20d6c8161d58820a78
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 11:59:26 -0400

    doing it for the rest?

--- src/components/Store2/Store2.js
+++ src/components/Store2/Store2.js
@@ -3,6 +3,7 @@ import Layout from '../Layout.js';
 import '../Global.css';
 import { Link } from 'react-router-dom';
 import {useSpring, animated} from 'react-spring'
+import { Mall, Item } from '../MallScript.js';
 
 function Store2() {
   const props = useSpring({
@@ -11,32 +12,25 @@ function Store2() {
     delay: 300,
   });
 
+  const myMall = new Mall("My Mall");
+  myMall.addItem(new Item("Product 1", 10));
+  myMall.addItem(new Item("Product 2", 20));
+  myMall.addItem(new Item("Product 3", 30));
+  myMall.addItem(new Item("Product 4", 40));
+
   return (
     <animated.div style={props}>
       <Layout>
         <div className="storeBox">
-          <h2>Store Name</h2>
+          <h2>{myMall.getMallName()}</h2>
           <ul>
-            <li>
-              <img src="product1.png" alt="Product 1" />
-              <h3><Link to="/mall2/product1">Product 1</Link></h3>
-              <p>$10.00</p>
-            </li>
-            <li>
-              <img src="product2.png" alt="Product 2" />
-              <h3><Link to="/mall2/product2">Product 2</Link></h3>
-              <p>$20.00</p>
-            </li>
-            <li>
-              <img src="product3.png" alt="Product 3" />
-              <h3><Link to="/mall2/product3">Product 3</Link></h3>
-              <p>$30.00</p>
-            </li>
-            <li>
-              <img src="product4.png" alt="Product 4" />
-              <h3><Link to="/mall2/product4">Product 4</Link></h3>
-              <p>$40.00</p>
-            </li>
+            {myMall.itemList.map((item, index) => (
+              <li key={index}>
+                <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
+                <h3><Link to={`/mall2/product${index + 1}`}>{item.getName()}</Link></h3>
+                <p>${item.getPrice().toFixed(2)}</p>
+              </li>
+            ))}
           </ul>
         </div>
       </Layout>
