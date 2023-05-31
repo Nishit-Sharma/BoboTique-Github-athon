e7a652b9c86ba3f3781713d54e6928ab6ceabad0
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 19:11:49 -0400

    no way this works

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -39,18 +39,21 @@ function Cart() {
   const displayItems = () => {
     const malls = [Mall1, Mall2, Mall3, Mall4];
     const items = malls.flatMap(mall => mall.itemList);
-    return items.map((item, index) => (
-      <animated.div style={props}>
-        <li key={index}>
-          <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
-          <h3>{item.getName()}</h3>
-          <p>${item.getPrice().toFixed(2)}</p>
-          <p>Mall: {findStore(item)}</p>
-          <button onClick={() => removeItem(item)}>Remove Item</button>
-        </li>
-      </animated.div>
-    ));
+    return (
+      <animated.ul style={props}>
+        {items.map((item, index) => (
+          <li key={index}>
+            <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
+            <h3>{item.getName()}</h3>
+            <p>${item.getPrice().toFixed(2)}</p>
+            <p>Mall: {findStore(item)}</p>
+            <button onClick={() => removeItem(item)}>Remove Item</button>
+          </li>
+        ))}
+      </animated.ul>
+    );
   }
+
 
   return (
     <animated.div style={props}>
