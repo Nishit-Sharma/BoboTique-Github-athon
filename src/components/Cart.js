98de2057a1f00ac3b8e23c6d6dd4a923c5a5b788
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 18:12:46 -0400

    plz workie

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -35,7 +35,7 @@ function Cart() {
         <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
         <h3>{item.getName()}</h3>
         <p>${item.getPrice().toFixed(2)}</p>
-        <p>Mall: {item.mall.getMallName()}</p>
+        <p>Mall: {item.mallName()}</p>
         <button onClick={() => removeItem(item)}>Remove Item</button>
         total += {item.getPrice()};
       </li>
