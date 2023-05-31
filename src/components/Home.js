5d0f30aebc93ba1e7ab22b3a0923ba8d77e4931f
Nishit-Sharma <nishit.sharma@mcvts.org>
2023-05-31 08:08:49 -0400

    x

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,33 +2,35 @@ import React from 'react';
 import './Global.css';
 import Layout from './Layout.js';
 import { Link } from 'react-router-dom';
-import {useSpring, animated} from 'react-spring'
+import { useSpring, animated } from 'react-spring';
 
 function Home() {
   const props = useSpring({
-    to: { opacity: 1},
-    from: { opacity: 0},
+    to: { opacity: 1 },
+    from: { opacity: 0 },
     delay: 300,
   });
+
   return (
     <animated.div style={props}>
       <Layout>
-      <h1>Welcome to our shopping website!</h1>
-      <p>Choose from our four malls to start shopping:</p>
-      <ul>
-        <li><Link to="/mall1">Mall1</Link></li>
-        <li><Link to="/mall2">Mall2</Link></li>
-        <li><Link to="/mall3">Mall3</Link></li>
-        <li><Link to="/mall4">Mall4</Link></li>
-      </ul>
-      <form>
-        <input type="text" placeholder="Search for products" />
-        <button type="submit">Search</button>
-      </form>
+        <div className="Home">
+          <h1>Welcome to our shopping website!</h1>
+          <p>Choose from our four malls to start shopping:</p>
+          <ul>
+            <li><Link to="/mall1">Mall1</Link></li>
+            <li><Link to="/mall2">Mall2</Link></li>
+            <li><Link to="/mall3">Mall3</Link></li>
+            <li><Link to="/mall4">Mall4</Link></li>
+          </ul>
+          <form>
+            <input type="text" placeholder="Search for products" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
       </Layout>
     </animated.div>
   );
-
 }
 
 export default Home;
