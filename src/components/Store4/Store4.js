89dc37dcec1335a323be5c4f5ec12117f9dbaf2d
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-05-31 08:52:52 -0400

    god help me

--- src/components/Store4/Store4.js
+++ src/components/Store4/Store4.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../Layout.js'; 
+import { Link } from 'react-router-dom';
 import {useSpring, animated} from 'react-spring'
 import '../Global.css';
 
@@ -16,22 +17,22 @@ function Store4() {
       <ul>
         <li>
           <img src="product1.png" alt="Product 1" />
-          <h3>Product 1</h3>
+          <h3><Link to="/mall4/product1">Product 1</Link></h3>
           <p>$10.00</p>
         </li>
         <li>
           <img src="product2.png" alt="Product 2" />
-         <h3>Product 2</h3>
+          <h3><Link to="/mall4/product2">Product 2</Link></h3>
           <p>$20.00</p>
         </li>
         <li>
           <img src="product3.png" alt="Product 3" />
-          <h3>Product 3</h3>
+          <h3><Link to="/mall4/product3">Product 3</Link></h3>
           <p>$30.00</p>
         </li>
         <li>
           <img src="product4.png" alt="Product 4" />
-          <h3>Product 4</h3>
+          <h3><Link to="/mall4/product4">Product 4</Link></h3>
           <p>$40.00</p>
         </li>
       </ul>
