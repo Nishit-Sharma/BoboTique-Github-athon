cf8650717743a4dda7b83c4a5623de471e8e2954
BrantisIsHacking <nishit.sharma@mcvts.org>
2023-06-03 11:21:05 -0400

    Everything Done (Besides Description & Prices)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import './Global.css';
 import Layout from './Layout.js';
-import {useSpring, animated} from 'react-spring'
+import { useSpring, animated } from 'react-spring';
 import { Mall1, Mall2, Mall3, Mall4, total } from './MallScript.js';
 
 function Cart() {
   const props = useSpring({
-    to: { opacity: 1},
-    from: { opacity: 0},
+    to: { opacity: 1 },
+    from: { opacity: 0 },
     delay: 300,
   });
 
@@ -15,16 +15,16 @@ function Cart() {
 
   const removeItem = (item) => {
     const malls = [Mall1, Mall2, Mall3, Mall4];
-    malls.forEach(mall => {
+    malls.forEach((mall) => {
       const index = mall.itemList.indexOf(item);
       if (index !== -1) {
         mall.removeItem(index);
       }
     });
-    const newCartItems = cartItems.filter(cartItem => cartItem !== item);
+    const newCartItems = cartItems.filter((cartItem) => cartItem !== item);
     setCartItems(newCartItems);
     total -= item.getPrice();
-  }
+  };
 
   const findStore = (item) => {
     const malls = [Mall1, Mall2, Mall3, Mall4];
@@ -34,37 +34,45 @@ function Cart() {
         return malls[i].getMallName();
       }
     }
-  }
+  };
 
   const displayItems = () => {
     const malls = [Mall1, Mall2, Mall3, Mall4];
-    const items = malls.flatMap(mall => mall.itemList);
+    const items = malls.flatMap((mall) => mall.itemList);
     return (
-      <ul>
+      <ul className="cart-item-list">
         {items.map((item, index) => (
-          <li key={index}>
-            <img src={`product${index + 1}.png`} alt={`Product ${index + 1}`} />
-            <h3>{item.getName()}</h3>
-            <p>${item.getPrice().toFixed(2)}</p>
-            <p>Store: {findStore(item)}</p>
-             <button onClick={() => removeItem(item)}>Remove Item</button>
-           </li>
+          <li key={index} className="cart-item">
+            <img
+              src={`product${index + 1}.png`}
+              alt={`Product ${index + 1}`}
+              className="cart-item-image"
+            />
+            <div className="cart-item-details">
+              <h4 className="cart-item-name">{item.getName()}</h4>
+              <p className="cart-item-price">${item.getPrice().toFixed(2)}</p>
+              <p className="cart-item-store">Store: {findStore(item)}</p>
+              <button
+                onClick={() => removeItem(item)}
+                className="cart-item-remove"
+              >
+                Remove Item
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
     );
-  }
-
+  };
 
   return (
     <animated.div style={props}>
       <Layout>
         <div className="Cart">
-          <h2>Shopping Cart</h2>
-          <ul>
-            {displayItems()}
-          </ul>
-          <p>Total: ${total.toFixed(2)}</p>
-          <button>Checkout</button>
+          <h2 className="cart-heading">Shopping Cart</h2>
+          {displayItems()}
+          <p className="cart-total">Total: ${total.toFixed(2)}</p>
+          <button className="cart-checkout-button">Checkout</button>
         </div>
       </Layout>
     </animated.div>
